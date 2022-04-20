10e69f8778f39e28ba495022caf1edb2fd08e4ed
Dani <79950554+Kaserola4@users.noreply.github.com>
2022-04-20 16:59:48 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const kas = new MissionCommander("Kas")
+const kaserola = new MissionCommander("Kaserola")
+const dani = new MissionCommander("Dani")
+console.log(kas.name)
+console.log(kaserola.name)
+console.log(dani.name)
